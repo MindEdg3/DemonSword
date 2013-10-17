9953d9db1f63270db6969775699dde28d8b3cde5
mrimsh <murkorp@gmail.com>
2013-10-18 02:18:16 +0400

    Basic AI

---
+++ Assets/Scripts/Game/AIController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIController : MonoBehaviour
+{
+	
+	private PlayerController _myPlayer;
+
+	private PlayerController MyPlayer {
+		get {
+			if (_myPlayer == null) {
+				_myPlayer = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ();
+			}
+			return _myPlayer;
+		}
+	}
+	
+	private Transform _myPlayerTr;
+
+	private Transform MyPlayerTr {
+		get {
+			if (_myPlayerTr == null) {
+				_myPlayerTr = MyPlayer.transform;
+			}
+			return _myPlayerTr;
+		}
+	}
+
+	private NavMeshAgent _myNavigation;
+
+	private NavMeshAgent MyNavigation {
+		get {
+			if (_myNavigation == null) {
+				_myNavigation = GetComponent<NavMeshAgent> ();
+			}
+			return _myNavigation;
+		}
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		MyNavigation.destination = MyPlayerTr.position;
+	}
+}
