eda046324c4c6bfdbf568ad5414f755d9c89ba65
mrimsh <murkorp@gmail.com>
2013-10-18 02:05:05 +0400

    Basic movement and camera follow

---
+++ Assets/Scripts/Game/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	private Camera _myCamera;
+	
+	public Camera MyCamera {
+		get {
+			if (_myCamera == null) {
+				_myCamera = Camera.main;
+			}
+			return _myCamera;
+		}
+	}
+
+	private Transform _myCameraTr;
+	
+	public Transform MyCameraTr {
+		get {
+			if (_myCameraTr == null) {
+				_myCameraTr = MyCamera.transform;
+			}
+			return _myCameraTr;
+		}
+	}
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetButtonDown ("Fire1")) {
+			Attack ();
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		Vector3 cameraRotation = MyCameraTr.rotation.eulerAngles;
+		Vector3 forceDirection = Quaternion.Euler (0f, cameraRotation.y, 0f) * new Vector3 (Input.GetAxis ("Horizontal"), 0f, Input.GetAxis ("Vertical")) * speed;
+		rigidbody.AddForce (forceDirection, ForceMode.Acceleration);
+		if (forceDirection != Vector3.zero) {
+			rigidbody.MoveRotation (Quaternion.LookRotation (forceDirection));
+		}
+	}
+
+	void Attack ()
+	{
+		Debug.Log ("ATTACK!!");
+	}
+}
---
+++ Assets/Scripts/Game/TPSCameraFollow.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TPSCameraFollow : MonoBehaviour
+{
+	public Transform target;
+	public Vector3 offset;
+	public Vector3 lookPoint;
+	public float followSpeed;
+	public float rotationDelay;
+	private float lastRotationTime = -1f;
+	private Quaternion lastTargetRotation;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+//		transform.position = Vector3.MoveTowards (transform.position, (target.position + (Quaternion.Euler (0f, target.rotation.eulerAngles.y, 0f) * offset)), followSpeed * Time.deltaTime);
+				
+//		if (lastRotationTime != -1) {
+//			if (lastRotationTime + rotationDelay > Time.time) {
+//			}
+//		}
+//		if (target.rotation != lastTargetRotation) {
+//			lastRotationTime = Time.time;
+//		}
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+	{
+		transform.position = target.position + (Quaternion.Euler (0f, transform.rotation.eulerAngles.y, 0f) * offset);
+		transform.LookAt (target.position + lookPoint);
+	}
+}
